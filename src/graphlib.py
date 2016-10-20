339deffb450debd58d2f7be0f62e0972d236c497
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2016-10-21 00:09:46 +0100

    added function to read file and return the adjancy matrix

--- src/graphlib.py
+++ src/graphlib.py
@@ -1,7 +1,24 @@
+import networkx as nx
 
+class node(object):
+    """docstring for node."""
+    def __init__(self, arg):
+        super(node, self).__init__()
+        self.arg = arg
+
+def read_file():
+    g = nx.read_gml('input/adjnoun.gml')
+    nodes = nx.nodes(g)
+    size = nx.number_of_nodes(g)
+    adj = [[0 for x in range(size)] for y in range(size)]
+    for node in nodes:
+        edges = nx.edges(g, node)
+        for edge in edges:
+            adj[ nodes.index(edge[0]) ][ nodes.index(edge[1]) ] = 1
+    return adj
 
 def main():
-
+    adj = read_file()
 
 if __name__ == "__main__":
     main()
