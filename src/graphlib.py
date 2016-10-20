b36bee8725dd886af5bc07701a71330213f04d17
Joao Godinho <joaogodinho_4@hotmail.com>
2016-10-20 23:07:50 +0100

    some comments

--- src/graphlib.py
+++ src/graphlib.py
@@ -1,13 +1,16 @@
 from __future__ import print_function
 
 
+
 def print_3d_array(array, n):
+	print("[print_3d_array]")
 	for i in range (n):
 		for j in range (n):
 			print (array[i][j], end=" ")
 		print ("")	
 
 def print_2d_array(array, n):
+	print("[print_2d_array]")
 	for i in range(n):
 		print(array[i], end=" ")
 	print("")
@@ -37,14 +40,23 @@ def get_degrees(adjacency_matrix, n):
 	return degrees_array
 
 def main():
+	#define adjacency_matrix test and its size
 	adjacency_matrix = [[0,1,1],[1,0,1],[1,1,0]]
 	n = 3;
-	print_3d_array(adjacency_matrix, n)	
 
+	#calculate degrees of nodes
 	degrees_array = get_degrees(adjacency_matrix, n)
-	print_2d_array(degrees_array, n)
+
+	#get the max degree of the list of degrees
 	max_degree = get_max_degree(degrees_array, n)
+	
+	#build the histogram relative to the list of degrees
 	histogram = build_histogram(degrees_array, n)
+
+	#print matrixes for test
+	print_3d_array(adjacency_matrix, n)	
+	print_2d_array(degrees_array, n)
+	print_2d_array(histogram, n)
 
 if __name__ == "__main__":
     main()
