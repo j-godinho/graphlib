7a6428d20bbf9897fae52252f15d9e6b36dc7202
Joao Godinho <joaogodinho_4@hotmail.com>
2016-10-26 21:27:49 +0100

    cleaning the prints

--- src/graphlib.py
+++ src/graphlib.py
@@ -2,15 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
+
 import random
 
-def print_2d_array(array):
-    print('[print_2dim_array]')
-    n = len(array)
-    for i in range(n):
-        for j in range(n):
-            print(array[i][j], end=' ')
-        print('')
 
 def create_random_graph(num_nodes, prob):
 	adj = [[0 for i in range(num_nodes)] for j in range(num_nodes)]
@@ -22,7 +16,6 @@ def create_random_graph(num_nodes, prob):
 					adj[i][j] = 1
 					adj[j][i] = 1
 
-	print_2d_array(adj)
 
 def main():
 	num_nodes = 20
