24ec49f8f1c28ff298bf2992e6ba8e6c18207793
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2016-11-01 19:48:14 +0000

    added a simple demo

--- demo.py
+++ demo.py
@@ -1,7 +1,12 @@
-import lib/graphlib
+import lib.graphlib as gl
 
 def main():
-    
+    # Generate a random model graph with 250 nodes and
+    # a probability of 0.05 of each node beeing conected
+    g = gl.generate_random_graph(250, 0.05)
+
+    # Print the clustering coefficient of the generated graph
+    print g.get_clustering_coefficient()
 
 
 if __name__ == '__main__':
